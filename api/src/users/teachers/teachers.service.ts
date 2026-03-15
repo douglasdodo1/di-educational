27e45a1bf7bbf7e214163439e60fb7ebe0879d44
Douglas Gemir <dcg@cin.ufpe.br>
2026-03-14 22:33:28 -0300

    feat(teachers.service): criando funções

--- api/src/users/teachers/teachers.service.ts
+++ api/src/users/teachers/teachers.service.ts
@@ -1,29 +1,34 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma.service';
-import { CreateTeacherDTO } from './dtos/create-teacher.dto';
-import type { Teacher } from 'src/generated/prisma/client';
+import { CreateTeacherInput } from './inputs/create-teacher.input';
+import { TeachersRepository } from './teachers.repository';
+import { TeacherModel } from './teachers.model';
+import { UpdateTeacherInput } from './inputs/update-teacher.input';
 
 @Injectable()
 export class TeachersService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private teachersRepository: TeachersRepository) {}
 
-  async create(data: CreateTeacherDTO): Promise<Teacher> {
-    return await this.prisma.teacher.create({
-      data: {
-        salary: data.salary,
-        user: {
-          create: {
-            email: data.email,
-            first_name: data.first_name,
-            last_name: data.last_name,
-            bio: data.bio,
-            password: data.password,
-          },
-        },
-      },
-      include: {
-        user: true,
-      },
-    });
+  async create(data: CreateTeacherInput): Promise<TeacherModel> {
+    return await this.teachersRepository.create(data);
+  }
+
+  async findAll(): Promise<TeacherModel[]> {
+    return await this.teachersRepository.findAll();
+  }
+
+  async findById(id: number): Promise<TeacherModel | null> {
+    return await this.teachersRepository.findById(id);
+  }
+
+  async update(
+    id: number,
+    data: UpdateTeacherInput,
+  ): Promise<TeacherModel | null> {
+    return await this.teachersRepository.update(id, data);
+  }
+
+  async delete(id: number): Promise<boolean> {
+    await this.teachersRepository.delete(id);
+    return true;
   }
 }
