a962ced25399ccb21d1f639f5592a82aaabc7872
Douglas Gemir <douglasgemir@gmail.com>
2026-03-14 22:33:53 -0300

    feat(create-teacher.input): criando arquivo

---
+++ api/src/users/teachers/inputs/create-teacher.input.ts
@@ -0,0 +1,8 @@
+import { Field, Float, InputType } from '@nestjs/graphql';
+import { CreateUserInput } from 'src/users/inputs/create-user.input';
+
+@InputType()
+export class CreateTeacherInput extends CreateUserInput {
+  @Field(() => Float)
+  salary!: number;
+}
