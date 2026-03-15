35145da5220795c495fbd30fbd6b534b966e8253
Douglas Gemir <dcg@cin.ufpe.br>
2026-03-15 11:16:58 -0300

    feat(students.resolver): adicionando mutations

--- api/src/users/students/students.resolver.ts
+++ api/src/users/students/students.resolver.ts
@@ -1,16 +1,42 @@
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Args, Int, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CreateStudentInput } from './inputs/create-student.input';
 import { StudentsService } from './students.service';
 import { StudentsModel } from './students.model';
+import { UpdateStudentInput } from './inputs/update.student.input';
 
 @Resolver(() => StudentsModel)
 export class StudentsResolver {
   constructor(private studentsService: StudentsService) {}
+
+  @Query(() => [StudentsModel])
+  students(): Promise<StudentsModel[]> {
+    return this.studentsService.findAll();
+  }
+
+  @Query(() => StudentsModel, { nullable: true })
+  student(
+    @Args('id', { type: () => Int }) id: number,
+  ): Promise<StudentsModel | null> {
+    return this.studentsService.findById(id);
+  }
 
   @Mutation(() => StudentsModel)
   createStudent(
     @Args('data', { type: () => CreateStudentInput }) data: CreateStudentInput,
   ) {
     return this.studentsService.create(data);
+  }
+
+  @Mutation(() => StudentsModel)
+  updateStudent(
+    @Args('id', { type: () => Int }) id: number,
+    @Args('data', { type: () => UpdateStudentInput }) data: UpdateStudentInput,
+  ) {
+    return this.studentsService.update(id, data);
+  }
+
+  @Mutation(() => Boolean)
+  deleteStudent(@Args('id', { type: () => Int }) id: number): Promise<boolean> {
+    return this.studentsService.delete(id);
   }
 }
