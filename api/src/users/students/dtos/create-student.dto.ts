b37469ddd8f04dae7e701b2983133aabcf29fcf4
Douglas Gemir <douglasgemir@gmail.com>
2026-03-14 22:30:15 -0300

    feat(create-student.dto): removendo arquivo

--- api/src/users/students/dtos/create-student.dto.ts
+++
@@ -1,8 +0,0 @@
-import { Field, InputType } from '@nestjs/graphql';
-import { CreateUserDTO } from 'src/users/create-user.dto';
-
-@InputType()
-export class CreateStudentDTO extends CreateUserDTO {
-  @Field()
-  enrollmentNumber!: string;
-}
