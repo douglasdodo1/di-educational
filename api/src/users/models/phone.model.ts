855140987f7cf0f032b86fdd4957e9e202d6e51a
Douglas Gemir <dcg@cin.ufpe.br>
2026-03-15 12:18:57 -0300

    feat(phone.model): criando arquivo

---
+++ api/src/users/models/phone.model.ts
@@ -0,0 +1,10 @@
+import { Field, ObjectType, Int } from '@nestjs/graphql';
+
+@ObjectType()
+export class Phone {
+  @Field(() => Int)
+  id!: number;
+
+  @Field(() => String)
+  number!: string;
+}
