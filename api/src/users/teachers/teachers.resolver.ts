3f2a09f1866675632bbb9c8e02b891665fb62adf
Douglas Gemir <douglasgemir@gmail.com>
2026-03-14 22:32:33 -0300

    feat(teachers.resolver): acrescentando querys

--- api/src/users/teachers/teachers.resolver.ts
+++ api/src/users/teachers/teachers.resolver.ts
@@ -1,19 +1,43 @@
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { Int } from '@nestjs/graphql';
 import { TeacherModel } from './teachers.model';
 import { TeachersService } from './teachers.service';
-import { CreateTeacherDTO } from './dtos/create-teacher.dto';
+import { CreateTeacherInput } from './inputs/create-teacher.input';
+import { UpdateTeacherInput } from './inputs/update-teacher.input';
 
 @Resolver(() => TeacherModel)
 export class TeachersResolver {
   constructor(private teachersService: TeachersService) {}
 
-  @Query(() => String)
-  hello(): string {
-    return 'Hello World';
+  @Query(() => [TeacherModel])
+  teachers(): Promise<TeacherModel[]> {
+    return this.teachersService.findAll();
+  }
+
+  @Query(() => TeacherModel, { nullable: true })
+  teacher(
+    @Args('id', { type: () => Int }) id: number,
+  ): Promise<TeacherModel | null> {
+    return this.teachersService.findById(id);
   }
 
   @Mutation(() => TeacherModel)
-  createTeacher(@Args('data') data: CreateTeacherDTO) {
+  createTeacher(
+    @Args('data', { type: () => CreateTeacherInput }) data: CreateTeacherInput,
+  ) {
     return this.teachersService.create(data);
+  }
+
+  @Mutation(() => TeacherModel)
+  updateTeacher(
+    @Args('id', { type: () => Int }) id: number,
+    @Args('data', { type: () => UpdateTeacherInput }) data: UpdateTeacherInput,
+  ) {
+    return this.teachersService.update(id, data);
+  }
+
+  @Mutation(() => Boolean)
+  deleteTeacher(@Args('id', { type: () => Int }) id: number): Promise<boolean> {
+    return this.teachersService.delete(id);
   }
 }
