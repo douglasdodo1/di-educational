614232cdf3b9a9ced0c568ba779c4911169000be
Douglas Gemir <douglasgemir@gmail.com>
2026-03-15 11:17:18 -0300

    feat(students.module): adicionando repository

--- api/src/users/students/students.module.ts
+++ api/src/users/students/students.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { StudentsService } from './students.service';
 import { StudentsResolver } from './students.resolver';
 import { PrismaService } from 'src/prisma.service';
+import { StudentsRepository } from './students.repository';
 
 @Module({
-  providers: [StudentsService, StudentsResolver, PrismaService],
+  providers: [
+    StudentsRepository,
+    StudentsService,
+    StudentsResolver,
+    PrismaService,
+  ],
 })
 export class StudentsModule {}
