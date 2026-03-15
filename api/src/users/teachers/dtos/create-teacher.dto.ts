9ae01f6dbee8782d058fcd9f1b039179732514e0
Douglas Gemir <dcg@cin.ufpe.br>
2026-03-14 22:32:48 -0300

    feat(create-teacher.dto): removendo arquivo

--- api/src/users/teachers/dtos/create-teacher.dto.ts
+++
@@ -1,8 +0,0 @@
-import { Field, Float, InputType } from '@nestjs/graphql';
-import { CreateUserDTO } from 'src/users/create-user.dto';
-
-@InputType()
-export class CreateTeacherDTO extends CreateUserDTO {
-  @Field(() => Float)
-  salary!: number;
-}
