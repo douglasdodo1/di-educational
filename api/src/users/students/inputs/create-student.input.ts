b1ff805bf28967b5f88399bb5f7c734d7223bf8f
Douglas Gemir <dcg@cin.ufpe.br>
2026-03-14 22:30:58 -0300

    feat(create-student.input): criando arquivo

---
+++ api/src/users/students/inputs/create-student.input.ts
@@ -0,0 +1,8 @@
+import { Field, InputType } from '@nestjs/graphql';
+import { CreateUserInput } from 'src/users/inputs/create-user.input';
+
+@InputType()
+export class CreateStudentInput extends CreateUserInput {
+  @Field()
+  enrollmentNumber!: string;
+}
