f4646259c4655bd2d5e9e899b6152975d797e662
Douglas Gemir <douglasgemir@gmail.com>
2026-03-21 15:32:24 -0300

    feat(classes.repository): removendo content de update

--- api/src/classes/classes.repository.ts
+++ api/src/classes/classes.repository.ts
@@ -1,22 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
-import { CreateClassInput } from './inputs/create.class.input';
+import { UpdateClassInput } from './inputs/update.class.input';
 
 @Injectable()
 export class classesRepository {
   constructor(private prisma: PrismaService) {}
 
-  async updateClass(classId: number, data: CreateClassInput) {
+  async updateClass(classId: number, data: UpdateClassInput) {
     await this.prisma.class.update({
       where: { id: classId },
       data: {
         name: data.name,
         description: data.description,
-        content: {
-          create: data.content,
-        },
       },
-      include: { content: true },
     });
   }
 
