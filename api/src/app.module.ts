fd07e60afa924fb8a955769ba4954fa30e2e5ef4
Douglas Gemir <douglasgemir@gmail.com>
2026-03-28 14:29:00 -0300

    feat(app.module): adicionando courses e classes

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ConfigModule } from '@nestjs/config';
+import { APP_FILTER } from '@nestjs/core';
 
 import { TeachersModule } from './users/teachers/teachers.module';
 import { StudentsModule } from './users/students/students.module';
 import { PrismaModule } from './prisma.module';
+import { PrismaExceptionFilter } from './common/filters/prisma.exception.filter';
+import { CoursesModule } from './Courses/courses.module';
+import { ClassesModule } from './classes/classes.module';
 
 @Module({
   imports: [
@@ -17,10 +21,12 @@ import { PrismaModule } from './prisma.module';
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql',
     }),
-
+    CoursesModule,
+    ClassesModule,
     TeachersModule,
     StudentsModule,
     PrismaModule,
   ],
+  providers: [{ provide: APP_FILTER, useClass: PrismaExceptionFilter }],
 })
 export class AppModule {}
