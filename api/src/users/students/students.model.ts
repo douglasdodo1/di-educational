84e29b107480f6cb9d616ca6347ced73ef735f64
Douglas Gemir <dcg@cin.ufpe.br>
2026-04-03 14:03:31 -0300

    feat(students.model.ts): alteração

--- api/src/users/students/students.model.ts
+++ api/src/users/students/students.model.ts
@@ -1,11 +1,14 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType, Int } from '@nestjs/graphql';
 import { UserModel } from '../models/users.model';
 
 @ObjectType()
 export class StudentsModel {
+  @Field(() => Int)
+  id!: number;
+
   @Field()
   enrollmentNumber!: string;
 
-  @Field(() => UserModel)
-  user!: UserModel;
+  @Field(() => UserModel, { nullable: true })
+  user?: UserModel | null;
 }
