fb037a30db5f04a392e7c5412fbb1decbc3853b5
Douglas Gemir <douglasgemir@gmail.com>
2026-04-03 14:03:32 -0300

    feat(students.service.ts): alteração

--- api/src/users/students/students.service.ts
+++ api/src/users/students/students.service.ts
@@ -3,13 +3,31 @@ import { CreateStudentInput } from './inputs/create.student.input';
 import { StudentsRepository } from './students.repository';
 import { UpdateStudentInput } from './inputs/update.student.input';
 import { StudentsModel } from './students.model';
+import { UsersService } from '../users.service';
 
 @Injectable()
 export class StudentsService {
-  constructor(private studentsRepository: StudentsRepository) {}
+  constructor(
+    private studentsRepository: StudentsRepository,
+    private usersService: UsersService,
+  ) {}
 
   async create(data: CreateStudentInput): Promise<StudentsModel> {
-    return await this.studentsRepository.create(data);
+    const { email, password, first_name, last_name, bio, phones } = data;
+
+    const user = await this.usersService.create({
+      email,
+      password,
+      first_name,
+      last_name,
+      bio,
+      phones,
+    });
+
+    return await this.studentsRepository.createWithUserId(
+      user.id,
+      data.enrollmentNumber,
+    );
   }
 
   async findAll() {
