37ba6af4f2bd6e3a5c33a31360e8f4fdab45d944
Douglas Gemir <douglasgemir@gmail.com>
2026-07-13 14:55:17 -0300

    style: format LoginForm code style

--- web/components/forms/auth/login/LoginForm.tsx
+++ web/components/forms/auth/login/LoginForm.tsx
@@ -1,18 +1,18 @@
-import { Input } from "@/components/ui/input";
-import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
-import { Button } from "@/components/ui/button";
-import { useLoginForm } from "./useLoginForm";
-import { Spinner } from "@/components/ui/spinner";
+import { Input } from '@/components/ui/input'
+import { Field, FieldError, FieldGroup, FieldLabel } from '@/components/ui/field'
+import { Button } from '@/components/ui/button'
+import { useLoginForm } from './useLoginForm'
+import { Spinner } from '@/components/ui/spinner'
 
 export const LoginForm = () => {
-  const form = useLoginForm();
+  const form = useLoginForm()
 
   return (
     <form
       onSubmit={(e) => {
-        e.preventDefault();
-        e.stopPropagation();
-        form.handleSubmit();
+        e.preventDefault()
+        e.stopPropagation()
+        form.handleSubmit()
       }}
       className="flex flex-col gap-4"
     >
@@ -21,7 +21,11 @@ export const LoginForm = () => {
           {(field) => (
             <Field>
               <FieldLabel className="text-base font-medium text-gray-700">Email</FieldLabel>
-              <Input className="py-5" value={field.state.value} onChange={(e) => field.handleChange(e.target.value)} />
+              <Input
+                className="py-5"
+                value={field.state.value}
+                onChange={(e) => field.handleChange(e.target.value)}
+              />
             </Field>
           )}
         </form.Field>
@@ -40,19 +44,21 @@ export const LoginForm = () => {
         </form.Field>
       </FieldGroup>
 
-      <div className="min-h-5 mx-auto">
+      <div className="mx-auto min-h-5">
         <form.Subscribe selector={(state) => [state.errorMap]}>
-          {([errorMap]) => (errorMap.onSubmit ? <FieldError>{errorMap.onSubmit.toString()}</FieldError> : null)}
+          {([errorMap]) =>
+            errorMap.onSubmit ? <FieldError>{errorMap.onSubmit.toString()}</FieldError> : null
+          }
         </form.Subscribe>
       </div>
 
       <form.Subscribe selector={(state) => [state.canSubmit, state.isSubmitting]}>
         {([canSubmit, isSubmitting]) => (
           <Button type="submit" disabled={!canSubmit || isSubmitting} className="h-12 bg-[#f05b05]">
-            {isSubmitting ? <Spinner /> : "Entrar"}
+            {isSubmitting ? <Spinner /> : 'Entrar'}
           </Button>
         )}
       </form.Subscribe>
     </form>
-  );
-};
+  )
+}
